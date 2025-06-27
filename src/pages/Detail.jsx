abd5d66392251fc2be914ec35d242ca2e2e7d221
Rifafradita12 <rifafraditasafara@gmail.com>
2025-06-27 19:24:05 +0700

    Fix folder name and Button

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Button from "../components/Button/Index";
-import StyledDetailMovie from "../components/UI/Button/DetailMovie.styled";
+import StyledDetailMovie from "../components/Button/DetailMovie.styled";
 function DetailMovie() {
   const {id} = useParams();
   const [movie, setMovie] = useState({});
